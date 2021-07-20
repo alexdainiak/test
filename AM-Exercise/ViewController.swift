d1f497fbc37318b5c9fb451b7ca429f4e22ca655
Michalis Mavris <m.mavris@arammeem.com>
2021-07-20 15:07:11 +0300

    Initial Commit

---
+++ AM-Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AM-Exercise
+//
+//  Created by Michael Mavris on 20/07/2021.
+//  Copyright © 2021 Michael Mavris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
